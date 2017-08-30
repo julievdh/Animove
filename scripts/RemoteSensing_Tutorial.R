5c32dc3f63488b2770bd0ac08892643eb7b9fba1
julievdh <julievdh@mit.edu>
2017-08-30 11:31:56 +0200

    Try with Azores data

--- scripts/RemoteSensing_Tutorial.R
+++ scripts/RemoteSensing_Tutorial.R
@@ -14,3 +14,19 @@ spplot(prec_ger1)
 prec_ger2 <- mask(prec_ger1, germany) # mask precipitation to shape of germany
 spplot(prec_ger2)
 
+# calculate average precipitation -- other statistics are possible too 
+prec_avg <- cellStats(prec_ger2,stat="mean") 
+
+## try precipitation in the Azores 
+portugal <- getData("GADM", country = "PRT", level = 1) # only need level 1 
+plot(portugal)
+aprec <- getData("worldclim",var = "prec", res = 0.5, lon = -25, lat = 37) # pricipitation data during these lats and lons
+# crop portugal to azores
+azores <- subset(portugal,NAME_1 == "Azores")
+plot(azores)
+
+prec_az1 <- crop(aprec,azores) # crop the precipitation to the azores 
+spplot(prec_az1)
+
+
+
